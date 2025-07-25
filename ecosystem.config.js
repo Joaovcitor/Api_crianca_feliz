8a8e4ff610dc2600231a97ad1f8b95f9a9ca566e
joaoVictor <neguchos@gmail.com>
2025-07-25 10:49:12 -0300

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,16 @@ module.exports = {
   apps: [
     {
       name: "minha-api",
-      script: "./src/server.ts",
-
-      exec_mode: "fork",
-      interpreter: "node",
-      interpreter_args: "-r ts-node/register",
+      script: "./dist/server.js", // <-- O PONTO CHAVE: Apontamos para o arquivo JS compilado!
+      instances: 1, // ou 'max' para usar todos os cores
       autorestart: true,
-      watch: false,
+      watch: false, // Nunca use 'watch' em produção com um build
       max_memory_restart: "1G",
+
+      // Define as variáveis de ambiente para produção
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
 };
