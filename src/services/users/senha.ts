e356dc9cd6b19c539693eb9838bcd3f62a7803db
joao victor <neguchos96@gmail.com>
2025-08-12 15:42:18 -0300

    fix arquivo senha

--- src/services/users/senha.ts
+++ src/services/users/senha.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 
-function validarSenha(password: string, confirmepassword: string) {
+export function validarSenha(password: string, confirmepassword: string) {
   if (password !== confirmepassword) {
     return { success: false, msg: "Senhas não são iguais!" };
   }
@@ -8,10 +8,8 @@ function validarSenha(password: string, confirmepassword: string) {
   return { success: true };
 }
 
-async function hashPassword(password: string) {
+export async function hashPassword(password: string) {
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
   return hashedPassword;
 }
-
-module.exports = { validarSenha, hashPassword };
