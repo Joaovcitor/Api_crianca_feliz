ccf6d6834cb968871f9d5ca78a82cb5b1b1a849e
joao victor <neguchos96@gmail.com>
2025-10-01 14:41:04 -0300

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,18 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.whiteList = listDomains;
+    this.whiteList = [
+      "https://primeirainfanciasuas.socialquixada.com.br",
+      "https://www.primeirainfanciasuas.socialquixada.com.br",
+      "https://172.31.48.1:4173",
+      "https://localhost:5173",
+      "http://localhost:3000",
+      "http://localhost:8100",
+      `http://localhost:4173`,
+      "http://localhost:5173",
+      "https://app-mobile-pcfv2-eight.vercel.app",
+      "https://mobilepcf.socialquixada.com.br",
+    ];
 
     this.configureMiddlewares();
     this.configureRoutes();
